5530ad4f57278e820a5bb7ff049c3d57874d5605
martin <martinkapo99@gmail.com>
2021-10-26 13:26:30 -0300

    buffer y carga de buffer

--- filtro.c
+++ filtro.c
@@ -2,49 +2,70 @@
 #include <stdlib.h>
 
 extern void enmascarar_asm(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant);
-void enmascarar_c(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant);
-int cargarBuffer(unsigned char *imagen, unsigned char *buffer);
+int enmascarar_c(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant);
+int cargarBuffer(unsigned char *imagen, unsigned char *buffer,int cant);
 
 int main(int argc, char *argv[])
 {
     unsigned char *img,*img2,*mask;
+    int alto,ancho,tamano;
     int RGB_size = 3;
     
     img = (unsigned char *)argv[1];
     img2 = (unsigned char *)argv[2];
     mask = (unsigned char *)argv[3];
-    int tamano = (int)argv[4] * (int)argv[5] * RGB_size; 
+    sscanf(argv[4],"%d",&alto);
+    sscanf(argv[5],"%d",&ancho);
+    tamano = alto * ancho * RGB_size;
+    enmascarar_c(img,img2,mask,tamano);
+
     return 0;
 }
 
-void enmascarar_c(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant)
+int enmascarar_c(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant)
 {
     unsigned char *buffer1,*buffer2,*buffermask;
 
-    buffer1 = (unsigned char*)malloc(cant);
-    buffer2 = (unsigned char*)malloc(cant);
-    buffermask = (unsigned char*)malloc(cant);
+    buffer1 = (unsigned char *)malloc(cant);
+    if (buffer1 ==NULL)
+    {
+        perror("Error al reservar memoria");
+        return 1;
+    }
+    buffer2 = (unsigned char *)malloc(cant);
+    if (buffer2 ==NULL)
+    {
+        perror("Error al reservar memoria");
+        return 1;
+    }
+    buffermask = (unsigned char *)malloc(cant);
+    if (buffermask ==NULL)
+    {
+        perror("Error al reservar memoria");
+        return 1;
+    }
+     
+    cargarBuffer(a,buffer1,cant);
+    cargarBuffer(b,buffer2,cant);
+    cargarBuffer(mask,buffermask,cant);
     
-    cargarBuffer(a,buffer1);
-    cargarBuffer(b,buffer2);
-    cargarBuffer(mask,buffermask);
+    free(buffer1);
+    free(buffer2);
+    free(buffermask); 
     
 }
 
-int cargarBuffer(unsigned char *imagen, unsigned char *buffer)
+int cargarBuffer(unsigned char *imagen, unsigned char * buffer,int cant_caracteres)
 {
     FILE *archivo = fopen(imagen,"rb");
 
-    if (archivo =NULL)
+    if (!archivo)
     {
         perror("Error al abrir archivo");
-        return 1;
+        return 2;
     }
 
-    fseek(archivo,0,SEEK_END);
-    int cant_caracteres = ftell(archivo);
-    rewind(archivo);
-
     fread(buffer,cant_caracteres,sizeof(unsigned char),archivo);
+    
     fclose(archivo);
 }
--- filtro.sh
+++ filtro.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 nasm -f elf filtro.s -o filtro.o
 gcc -m32 filtro.o filtro.c -o filtro
+./filtro "imagen1.rgb" "imagen2.rgb" "mask.rgb" 1098 732
 
