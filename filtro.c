5494900116090b5a7a1566aad624636faaf9d71e
martin <martinkapo99@gmail.com>
2021-10-31 11:30:31 -0300

    medir tiempo y resize en c

--- filtro.c
+++ filtro.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 extern void enmascarar_asm(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant);
 void enmascararAssembler(unsigned char *a ,unsigned char *b ,unsigned char *mask,int cant);
@@ -12,16 +13,17 @@ int main(int argc, char *argv[])
     unsigned char *img,*img2,*mask;
     unsigned char *buffer1,*buffer2,*buffermask;
     int alto,ancho,imagenSize;
+    clock_t inicio,fin;
+    double tiempoC,tiempoAsm;
+
     int RGB_size = 3; //en bytes
-    
     img = (unsigned char *)argv[1];
     img2 = (unsigned char *)argv[2];
     mask = (unsigned char *)argv[3];
     sscanf(argv[4],"%d",&alto);
     sscanf(argv[5],"%d",&ancho);
-    imagenSize = alto * ancho * RGB_size;
     
-
+    imagenSize = alto * ancho * RGB_size;
     buffer1 = (unsigned char *)malloc(imagenSize);
     buffer2 = (unsigned char *)malloc(imagenSize);
     buffermask = (unsigned char *)malloc(imagenSize); 
@@ -29,13 +31,24 @@ int main(int argc, char *argv[])
     cargarBuffer(img,buffer1,imagenSize);
     cargarBuffer(img2,buffer2,imagenSize);
     cargarBuffer(mask,buffermask,imagenSize);
-
+     
+    inicio=clock();
     enmascarar_c(buffer1,buffer2,buffermask,imagenSize);
-    //enmascararAssembler(buffer1,buffer2,buffermask,imagenSize);
+    fin=clock();
+    tiempoAsm = (double)(fin-inicio)/CLOCKS_PER_SEC;
+    printf("Imagen tamano: %d x %d\n",alto,ancho);
+    printf("Tiempo en C : %.6f\n",tiempoAsm);
+
+    inicio=clock();
+    enmascararAssembler(buffer1,buffer2,buffermask,imagenSize);
+    fin=clock();
+    tiempoC = (double)(fin-inicio)/CLOCKS_PER_SEC;
+    printf("Imagen tamano: %d x %d\n",alto,ancho);
+    printf("Tiempo en ASM : %.6f\n",tiempoC);
+    
     free(buffer1);
     free(buffer2);
-    free(buffermask); 
-
+    free(buffermask);
     return 0;
 }
 
@@ -66,7 +79,6 @@ int enmascarar_c(unsigned char *buff1 ,unsigned char *buff2 ,unsigned char *buff
         }
         i+=3;
     }
-    printf("valor de i: %d",i);
 
     FILE *archivo_C = fopen("salida.rgb","wb");
     fwrite(buff1,cant,1,archivo_C);
--- filtro.sh
+++ filtro.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 nasm -f elf filtro.s -o filtro.o
 gcc -m32 filtro.o filtro.c -o filtro
+
+gm convert paisaje.jpg paisaje.rgb
+gm convert lobo.jpg lobo.rgb
+gm convert boceto.jpg boceto.rgb
+
+./filtro "paisaje.rgb" "lobo.rgb" "boceto.rgb" 3888 2592
+gm convert -size 3888x2592 -depth 8  salida.rgb  salida.jpg
+gm convert -size 3888x2592 -depth 8  salida2.rgb  salida2.jpg
+
+rm paisaje.rgb lobo.rgb boceto.rgb salida.rgb salida2.rgb 
+
+<< 'comentario'
+#---------------------------------------------------
+gm convert -resize 1098x732 paisaje.jpg imagen1.jpg
+gm convert -resize 1098x732 lobo.jpg imagen2.jpg
+gm convert -resize 1098x732 boceto.jpg mask.jpg
+
+gm convert imagen1.jpg imagen1.rgb
+gm convert imagen2.jpg imagen2.rgb
+gm convert mask.jpg mask.rgb
+
 ./filtro "imagen1.rgb" "imagen2.rgb" "mask.rgb" 1098 732
 gm convert -size 1098x732 -depth 8  salida.rgb  salida.jpg
-#gm convert -size 1098x732 -depth 8  salida2.rgb  salida2.jpg
+gm convert -size 1098x732 -depth 8  salida2.rgb  salida2.jpg
 
+rm imagen1.rgb imagen2.rgb mask.rgb salida.rgb salida2.rgb
+
+#---------------------------------------------------
+
+gm convert -resize 500x300 paisaje.jpg imagen1.jpg
+gm convert -resize 500x300 lobo.jpg imagen2.jpg
+gm convert -resize 500x300 boceto.jpg mask.jpg
+
+gm convert imagen1.jpg imagen1.rgb
+gm convert imagen2.jpg imagen2.rgb
+gm convert mask.jpg mask.rgb
+
+./filtro "imagen1.rgb" "imagen2.rgb" "mask.rgb" 500 300
+gm convert -size 500x300 -depth 8  salida.rgb  salida.jpg
+gm convert -size 500x300 -depth 8  salida2.rgb  salida2.jpg
+
+rm imagen1.rgb imagen2.rgb mask.rgb salida.rgb salida2.rgb
+comentario
